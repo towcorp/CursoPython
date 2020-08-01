0555c8f00a1f331e49ec6a9a2ac23287af18642b
towcorp <towcorp@hotmail.com>
2020-08-01 17:08:16 -0300

    Arrumando a identacao

--- mediaAluno.py
+++ mediaAluno.py
@@ -1,8 +1,14 @@
 #Criando uma aplicação que determina se o aluno foi aprovado ou não de acordo com a média
+
 print('Bem vindo a aplicação\n')
+
 a = int(input('Quantidade de provas: '))  # TOTAL DE AVALIAÇÕES DADA
 soma = 0
+
+
 print('------------------------------\n')
+
+
 for i in range(a):
     nota = float(input('Digite a {}º nota: '.format(i+1)))  # DIGITAR A NOTA EM FORMATO DECIMAL
     soma = soma + nota
@@ -13,9 +19,15 @@ result = media * 10
 
 if result > 100.0:  # No caso as notas serem de 0 a 100
     result = media
+
+
 print('\n------------------------------')
+
 print('Resultado final é: {}% \nSua media é: {}'.format(round(result, 2), media))
+
 print('------------------------------')
+
+
 if result >= 70.0:
     print('\nParabés voce está APROVADO')
 
